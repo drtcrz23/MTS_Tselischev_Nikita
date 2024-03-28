aecac678c5a2336dd2c4a5dbb11e5c649e49b6fc
Nikita Tselischev <tselishchev.na@phystech.edu>
2024-03-28 17:34:58 +0300

    add author-registry-service

---
+++ author-registry-service/src/main/java/com/example/authorregistryservice/AuthorRegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.authorregistryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorRegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorRegistryServiceApplication.class, args);
+	}
+
+}
---
+++ author-registry-service/src/test/java/com/example/authorregistryservice/AuthorRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authorregistryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorRegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
